4159693c6ecf2cdfea66bd949dedb06f6dd6268c
Noah <noahmwangi@gmail.com>
2023-03-15 20:03:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  let ride;
+  if (feet <= 400) {
+    ride = "This one is on me!";
+  }
+  else if (feet > 400 && feet <= 2000) {
+    ride = "This one is on me too!";
+  }
+  else if (feet > 2000 && feet <=2500) {
+    ride = 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+    ride = 'No can do.'
+  }
+  return ride;
+} 
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let destination;
+  if (city === "NYC") {
+    destination = "Ok, sounds good.";
+  }
+  else {
+    destination = "No go.";
+  }
+  return destination;
 }
-
-function switchOnCharmFromTip(){
+const tip ="generous";
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch (tip) {
+    case "generous":
+      charm = "Thank you so much.";
+      break;
+    case "not as generous":
+        charm = "Thank you.";
+      break;
+    default:
+        charm = "Bye.";
+      break;
+  }
+  return charm;
   // Write your code here!
 }
